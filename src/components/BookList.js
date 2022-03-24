2366284d93aacf89f98132aa6764f7c32922e647
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-03-25 01:19:05 +0430

    initialize bookstore react componenets and basic styles

---
+++ src/components/BookList.js
@@ -0,0 +1,42 @@
+import '../assets/BookList.css';
+import { v4 as uuidv4 } from 'uuid';
+import Books from './Books';
+import AddNew from './AddNew';
+
+const BookList = () => {
+  const store = [
+    {
+      category: 'Action',
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      category: 'Science Fiction',
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      category: 'Economy',
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ];
+  return (
+    <div className="book-list">
+      <ul className="book">
+        {store.map((book) => (
+          <Books
+            category={book.category}
+            title={book.title}
+            author={book.author}
+            key={uuidv4()}
+          />
+        ))}
+      </ul>
+      <hr />
+      <AddNew />
+    </div>
+  );
+};
+
+export default BookList;
---
+++ src/components/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Categories = () => (
+  <div>
+    <h3>Coming soon...</h3>
+  </div>
+);
+
+export default Categories;
