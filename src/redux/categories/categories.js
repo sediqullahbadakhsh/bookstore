b7c1fcd422cbceb1c9d297af6060c37e86edcc89
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-03-30 20:53:04 +0430

    adding actions and reducer

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,18 @@
+const STATUS = 'STATUS';
+const INITIAL_STATE = [];
+
+export function checkStatus(id) {
+  return {
+    type: STATUS,
+    payload: id,
+  };
+}
+
+export default function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case STATUS:
+      return 'Under construction';
+    default:
+      return state;
+  }
+}
