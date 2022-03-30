79e1a604f5fda975b483313ba03cc327b735016a
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-03-30 22:50:16 +0430

    solve default reducer issue

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers } from 'redux';
 import * as Books from './books/books';
 import * as Categories from './categories/categories';
 
-const rootReducer = combineReducers(Books.default, Categories.default);
+const rootReducer = combineReducers(Books, Categories);
 const store = createStore(rootReducer);
 
 export default store;
